6b2e9d7783f6e1d94c4854979656390491884a09
killmeorgetout <54769203+killmeorgetout@users.noreply.github.com>
2019-11-01 14:24:16 +0800

    Update Server.java

--- Client/Server.java
+++ Client/Server.java
@@ -1,3 +1,4 @@
+
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -38,16 +39,17 @@ public class Server extends JFrame implements ActionListener {
 	ArrayList<ServerThread> playerlist = new ArrayList<ServerThread>();
 
 	// 花色
-	String[] color = { "c", "d", "s", "h" };
+	String[] color = { "c", "d", "s", "h" ,"c", "d", "s", "h"};
 
 	// 牌数字
 	String[] num = { "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13",
 			"14", "15" };
 
 	// 随机分成的四组
-	String[][] player = new String[4][25];//4个人
-
-	Server() {//服务器GUI
+	String[][] player = new String[4][25];
+	String[] pai= new String[108];
+	String[] dizhupai= new String[8];
+	Server() {
 		setTitle("服务器");
 		setSize(400, 500);
 		setLocationRelativeTo(null);
@@ -70,6 +72,7 @@ public class Server extends JFrame implements ActionListener {
 
 	public void init() {
 		text = new JTextArea();
+		
 		String myaddress = null;
 		try {
 			myaddress = InetAddress.getLocalHost().getHostAddress().toString(); // 服务器IP地址
@@ -83,28 +86,116 @@ public class Server extends JFrame implements ActionListener {
 		close = new JButton("关闭服务器");
 		add(close, BorderLayout.SOUTH);
 		close.addActionListener(this);
+		
 	}
 
-	
-	
-	
 	/*
 	 * 随机分成四组牌分发给玩家
 	 */
 	public void Randomization() {
-		int[] a = new int[4];
-		for (int i = 0; i < 4; i++)
-			a[i] = 0;//标记每个人牌的数量
+		
+	  int n=0;
 		for (int i = 0; i < 13; i++)//牌的点数
-			for (int j = 0; j < 4; j++) {//牌的花色
-				int row = (new Random().nextInt(100)) % 4;//随机的人
-				if (a[row] < 25)
-					player[row][a[row]++] = "" + color[j] + num[i];
-				else {
-					while (a[row] >= 25)//牌发满了就顺延续到下个人
-						row = (row + 1) % 4;
-					player[row][a[row]++] = "" + color[j] + num[i];
-				}
+			for (int j = 0; j <8; j++) {//牌的花色
+				pai[n++]=	 color[j] + num[i];}
+		
+		pai[n++]="b1";
+		pai[n++]="b2";
+		pai[n++]="r1";
+		pai[n++]="r2";
+		
+		int [] ischoose=new int[108];
+		for (int i = 0; i < 108; i++) 
+			ischoose[i]=0;//标记是否被发
+		
+		
+		
 
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 25; j++) {
+			int row = (new Random().nextInt(108));
+			if(ischoose[row]==0) {
+			player[i][j]=pai[row];
+			ischoose[row]=1;
 			}
+			else {
+				while(ischoose[row]==1) {
+				row = (new Random().nextInt(108));
+				}//一定要发出这张派
+				player[i][j]=pai[row];
+					
+				}
+				
+			}
+			}	
+		 
+		for (int i = 0; i < 108; i++) {//找出地主牌
+			int n1=0;
+			if(ischoose[i]==0) {
+				dizhupai[n1++]=pai[i];
+				
+			}
+			
+			
+			
+		}
+	
 	}
+	
+	
+	public void startServer(int port) {
+		try {
+			server = new ServerSocket(port);
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null, "服务器已启动！", "提示",
+					JOptionPane.WARNING_MESSAGE);
+			System.exit(0);
+		}
+		while (true) {
+			try {
+				you = server.accept();
+				youraddress = you.getInetAddress();
+			} catch (IOException e) {
+			}
+			if (you != null) {
+				new ServerThread(you, youraddress).start();
+			}
+		}
+	}
+
+	class ServerThread extends Thread {
+		Socket socket;
+
+		InetAddress address;
+
+		DataInputStream is = null;
+
+		DataOutputStream os = null;
+
+		String id = null, psw = null; // 用户信息
+
+		int tablenum; // 用户进入的table号
+
+		int seatnum; // table的seat号
+
+		/*
+		 * 该线程的位置属性,"hall"表示大厅线程，"seat"表示seat线程,
+		 * 一个player刚登陆时启动一个线程，由于socket不能共享，故在
+		 * 进入seat后的线程和刚刚进入大厅的线程标志区别就在location上
+		 */
+		String location = null;
+
+		boolean readyflag = false; // 游戏是否准备好
+
+		ServerThread(Socket s, InetAddress addr) {
+			tablenum = -1;
+			seatnum = -1;
+			socket = s;
+			location = "hall";
+			address = addr;
+			try {
+				is = new DataInputStream(socket.getInputStream());
+				os = new DataOutputStream(socket.getOutputStream());
+			} catch (IOException e) {
+			}
+		}
