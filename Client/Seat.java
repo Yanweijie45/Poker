bcde0d5a4d26e88006b0b18e82c31f1198024f84
killmeorgetout <54769203+killmeorgetout@users.noreply.github.com>
2019-11-01 14:25:01 +0800

    Create Seat.java
    座位类

---
+++ Client/Seat.java
@@ -0,0 +1,12 @@
+import javax.swing.JLabel;
+
+public class Seat extends JLabel{
+	public boolean state;
+	public int tablenum;
+	public int seatnum;
+	public Seat(){
+		state=false;
+		tablenum=-1;
+		seatnum=-1;
+	}
+}
